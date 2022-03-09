6dc31a9012f3141f86960ac25501b3c888dd7b17
Djambulat <Djambulat69@gmail.com>
2022-03-09 13:53:37 +0300

    Init

---
+++ app/src/main/java/com/isaev/menutest/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.isaev.menutest
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
+import com.isaev.menutest.ui.theme.MenuTestTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
+        setContent {
+            MenuTestTheme {
+                Surface(modifier = Modifier.fillMaxSize() , color = MaterialTheme.colors.background) {
+
+                }
+            }
+        }
+    }
+}

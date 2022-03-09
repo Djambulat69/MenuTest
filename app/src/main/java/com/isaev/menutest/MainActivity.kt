f3d2d0ca522ef4916f98b998ea33332fe6a04583
Djambulat <Djambulat69@gmail.com>
2022-03-09 16:14:19 +0300

    added menu items (without spaces)

--- app/src/main/java/com/isaev/menutest/MainActivity.kt
+++ app/src/main/java/com/isaev/menutest/MainActivity.kt
@@ -9,13 +9,13 @@ import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Settings
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.core.view.WindowCompat
 import com.isaev.menutest.ui.theme.MenuTestTheme
@@ -37,6 +37,26 @@ class MainActivity : ComponentActivity() {
                             contentScale = ContentScale.FillWidth,
                             modifier = Modifier.fillMaxWidth()
                         )
+                        MenuItem(
+                            name = stringResource(R.string.settings),
+                            icon = painterResource(R.drawable.ic_settings)
+                        )
+                        MenuItem(
+                            name = stringResource(R.string.tarifes),
+                            icon = painterResource(R.drawable.ic_shopping_bag)
+                        )
+                        MenuItem(
+                            name = stringResource(R.string.help),
+                            icon = painterResource(R.drawable.ic_question)
+                        )
+                        MenuItem(
+                            name = stringResource(R.string.about_app),
+                            icon = painterResource(R.drawable.ic_info)
+                        )
+                        MenuItem(
+                            name = stringResource(R.string.privacy_policy),
+                            icon = painterResource(R.drawable.ic_lock_open)
+                        )
                     }
                 }
             }
@@ -45,7 +65,7 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MenuItem(name: String, painter: Painter) {
+fun MenuItem(name: String, icon: Painter) {
     Row(
         modifier = Modifier
             .fillMaxWidth()
@@ -53,10 +73,11 @@ fun MenuItem(name: String, painter: Painter) {
             .padding(vertical = 12.dp, horizontal = 16.dp)
     ) {
         Icon(
-            imageVector = Icons.Filled.Settings,
-            contentDescription = "settings"
+            painter = icon,
+            contentDescription = name,
+            tint = Color.Unspecified
         )
         Spacer(Modifier.width(16.dp))
-        Text(text = "Settings")
+        Text(text = name)
     }
 }
